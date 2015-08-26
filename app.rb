2a987f528fa3645a574e6cc64ed126411ab0bb43
Robby Ranshous <rranshous@gmail.com>
2015-08-26 19:36:37 -0400

    init

---
+++ app.rb
@@ -0,0 +1,14 @@
+# A game which is simple
+# in the form of a simulation
+# where you provide the chance # of the player
+# taking actions
+
+# GAME: find the ball
+# you are on a grid
+# somewhere on that grid is a ball
+# you can walk north, east, south or west
+# you can choose to move in more than one direction
+# if you occupy the same cell as the ball, you have found it
+
+
+
---
+++ simulation.rb
@@ -0,0 +1,69 @@
+class Simulation
+
+  @@board_size = 10
+
+  def initialize move_chance
+    @move_chance = move_chance.map(&:to_i)
+    @player_location = [0,0]
+    @ball_location = [0,0]
+    randomize_board
+  end
+
+  def cycle
+    if game_over?
+      raise "GAME OVER"
+    end
+
+    [[0,1],[1,0],[0,-1],[-1,0]].each_with_index do |vector, i|
+      if rand(100) < @move_chance[i]
+        @player_location[0] += vector[0]
+        @player_location[1] += vector[1]
+      end
+    end
+
+    keep_player_on_board
+    print_board
+  end
+
+  def keep_player_on_board
+    @player_location[0] = [0, [@player_location[0], @@board_size-1].min].max
+    @player_location[1] = [0, [@player_location[1], @@board_size-1].min].max
+  end
+
+  def game_over?
+    @player_location == @ball_location
+  end
+
+  def print_board
+    @@board_size.times do |x|
+      @@board_size.times do |y|
+        if [x,y] == @player_location
+          print '*'
+        elsif [x,y] == @ball_location
+          print '.'
+        else
+          print '#'
+        end
+      end
+      puts
+    end
+    puts
+  end
+
+  private
+
+  def randomize_board
+    @player_location = [rand(@@board_size), rand(@@board_size)]
+    @ball_location = [rand(@@board_size), rand(@@board_size)]
+  end
+end
+
+
+#puts 'starting'
+#sim = Simulation.new(ARGV[0..3])
+#sim.print_board
+#loop do
+#  sim.cycle
+#  sleep(1)
+#end
+#puts 'ending'
